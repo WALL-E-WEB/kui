147f733a1696532210ae76015cc504d86bc53bbb
walle <2935661624@qq.com>
2022-12-07 22:38:30 +0800

    first commit

---
+++ lib/src/components/button/button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class KButton extends StatelessWidget {
+  const KButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      child: Text('data'),
+    );
+  }
+}
